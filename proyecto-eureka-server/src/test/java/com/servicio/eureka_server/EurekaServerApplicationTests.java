77188f1aec09e8e533bf7ccad2244ff90726c64b
soyMatiasAcosta <matias.acostacarpio@gmail.com>
2026-06-15 05:39:58 -0500

    integrado Eureka-server y gateway de Adria y ya fueron levantados y funcionan en puerto local

--- proyecto-eureka-server/src/test/java/com/servicio/eureka_server/EurekaServerApplicationTests.java
+++ proyecto-eureka-server/src/test/java/com/servicio/eureka_server/EurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.cibertec.reserva;
+package com.servicio.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProyectoEurekaServerApplicationTests {
+class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
